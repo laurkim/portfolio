0e5a23f1c8b93d8a647a150b989fb0ef808c23fa
laurkim <laurakeem@gmail.com>
2018-11-06 14:56:34 -0500

    Add routing and links for navbar

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import {hexagon} from '../constants'
+import { hexagon } from '../Constants';
+import '../App.css';
+import { BrowserRouter as Route, Link } from "react-router-dom";
+
 
 const Header = () => {
   return (
-    <img src={hexagon} alt="hexagon-logo"/>
+    <div>
+      <img id="hexagon-logo" src={hexagon} alt="hexagon-logo"/>
+      <div id="nav-bar">
+        <Link to="www.google.com" target="_blank">Projects</Link>
+        <Link to="www.google.com" target="_blank">Resume</Link>
+        <Link to="www.google.com" target="_blank">About</Link>
+      </div>
+    </div>
   )
 };
 
--- src/Container/App.js
+++ src/Container/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import '../App.css';
 import Header from '../Components/Header';
+import { BrowserRouter as Router } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div id="container">
-        <Header />
+        <Router>
+          <Header />
+        </Router>
       </div>
     );
   }
