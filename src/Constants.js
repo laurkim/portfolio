e05e65f975c041cadffdd9842652e3374d26c0bf
laurkim <laurakeem@gmail.com>
2018-11-07 14:02:30 -0500

    Add assets for projects and format project data

--- src/Constants.js
+++ src/Constants.js
@@ -3,14 +3,17 @@ export const hexagon = "./assets/hexagon-transparent.png";
 export const projects = {
     "dwell": {
         live_url: "https://dwell-here.herokuapp.com/",
-        demo_url: "https://github.com/laurkim/dwell-demo"
+        demo_url: "https://github.com/laurkim/dwell-demo",
+        splash_image: "./assets/dwell-splash.png"
     },
     "sonder": {
         live_url: "http://sonder-app.herokuapp.com/",
-        demo_url: "https://github.com/laurkim/sonder-demo"
+        demo_url: "https://github.com/laurkim/sonder-demo",
+        splash_image: "./assets/sonder-splash.png"
     },
     "cognizance": {
         live_url: "https://cognizance-game.herokuapp.com/",
-        demo_url: "https://github.com/laurkim/cognizance-demo"
+        demo_url: "https://github.com/laurkim/cognizance-demo",
+        splash_image: "./assets/cognizance-splash.png"
     }
 }
